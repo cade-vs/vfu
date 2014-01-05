b68c0d1424ee7ef03304d3ea4c1710673fd4e65d
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2014-01-05 13:16:05 +0200

    2014

--- vfuopt.cpp
+++ vfuopt.cpp
@@ -1,11 +1,11 @@
-/*
+/****************************************************************************
  *
  * (c) Vladi Belperchinov-Shabanski "Cade" 1996-2014
  * http://cade.datamax.bg/  <cade@biscom.net> <cade@bis.bg> <cade@datamax.bg>
  *
  * SEE `README',`LICENSE' OR `COPYING' FILE FOR LICENSE AND OTHER DETAILS!
  *
- */
+ ****************************************************************************/
 
 #include "vfu.h"
 #include "vfuopt.h"
--- vfuopt.h
+++ vfuopt.h
@@ -1,11 +1,11 @@
-/*
+/****************************************************************************
  *
- * (c) Vladi Belperchinov-Shabanski "Cade" 1996-2012
+ * (c) Vladi Belperchinov-Shabanski "Cade" 1996-2014
  * http://cade.datamax.bg/  <cade@biscom.net> <cade@bis.bg> <cade@datamax.bg>
  *
  * SEE `README',`LICENSE' OR `COPYING' FILE FOR LICENSE AND OTHER DETAILS!
  *
- */
+ ****************************************************************************/
 
 #ifndef _VFUOPT_H_
 #define _VFUOPT_H_
