bb8f4589a163aba7e5c6188e7e57e604ef4f4517
Vladi Belperchinov-Shabanski <cade@bis.bg>
2019-12-09 02:26:59 +0200

    maint-2019

--- vfusetup.h
+++ vfusetup.h
@@ -20,7 +20,7 @@
 
 #define VFU_VERSION "4.18"
 
-#define HEADER "VF/U v" VFU_VERSION " (" __DATE__ ") by (c) Vladi Belperchinov-Shabanski `Cade' 1996-2018"
+#define HEADER "VF/U v" VFU_VERSION " (" __DATE__ ") by (c) Vladi Belperchinov-Shabanski `Cade' 1996-2019"
 #define CONTACT "<cade@biscom.net> <cade@datamax.bg> [http://cade.datamax.bg]"
 
 #ifdef _TARGET_GO32_
